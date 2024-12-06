f10619b64fe55dac6fdbae08b6cb10ac8d77defb
Gabriel <gabriel.g2d2@gmail.com>
2024-12-06 12:42:25 -0300

    refactor: 🔧 Use env module for DATABASE_URL in database configuration

--- src/database.ts
+++ src/database.ts
@@ -1,12 +1,11 @@
 import "dotenv/config";
 import { type Knex, knex as setupKnex } from "knex";
-
-if (!process.env.DATABASE_URL) throw new Error("DATABASE_URL not defined");
+import { env } from "./env";
 
 export const configKnex: Knex.Config = {
   client: "sqlite",
   connection: {
-    filename: process.env.DATABASE_URL,
+    filename: env.DATABASE_URL,
   },
   useNullAsDefault: true,
   migrations: {
