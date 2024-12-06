bc31fcbe10472b5f61a9589f3fde32d440a8529c
Gabriel <gabriel.g2d2@gmail.com>
2024-12-06 16:09:11 -0300

    feat: ✨ Add POST endpoint for creating transactions with validation

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,10 +1,50 @@
 import type { FastifyInstance } from "fastify";
+import { randomUUID } from "node:crypto";
+import { z } from "zod";
 import { knex } from "../database";
+import { ITypeMessageGlobal } from "../global/types/typeMessage";
 
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.get("/hello", async function handler() {
-    const transactions = await knex("transactions").select("*");
+  app.post("/transactions", async (request, response) => {
+    const createTransactionSchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(["credit", "debit"]),
+    });
 
-    return transactions;
+    const verifyBody = createTransactionSchema.safeParse(request.body);
+
+    if (!verifyBody.success) {
+      return response.status(400).send({
+        data: null,
+        message: {
+          en: "Invalid data",
+          pt: "Dados inválidos",
+        },
+        typeMessage: ITypeMessageGlobal.ERROR,
+        errors: verifyBody.error.errors,
+      });
+    }
+
+    const body = verifyBody.data;
+
+    await knex("transactions").insert({
+      id: randomUUID(),
+      title: body.title,
+      amount: body.type === "credit" ? body.amount : -body.amount,
+    });
+
+    return response.status(201).send({
+      data: {
+        title: body.title,
+        amount: body.amount,
+        type: body.type,
+      },
+      message: {
+        en: "Transaction created successfully",
+        pt: "Transação criada com sucesso",
+      },
+      typeMessage: ITypeMessageGlobal.SUCCESS,
+    });
   });
 }
