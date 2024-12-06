db1297f34ac8e974886bd8f4fb0863a3d8e56371
Gabriel <gabriel.g2d2@gmail.com>
2024-12-06 12:28:42 -0300

    feat: ✨ Validate DATABASE_URL environment variable in database configuration

--- src/database.ts
+++ src/database.ts
@@ -1,6 +1,8 @@
 import "dotenv/config";
 import { type Knex, knex as setupKnex } from "knex";
 
+if (!process.env.DATABASE_URL) throw new Error("DATABASE_URL not defined");
+
 export const configKnex: Knex.Config = {
   client: "sqlite",
   connection: {
