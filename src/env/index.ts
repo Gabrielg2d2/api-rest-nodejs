3f1c6e30cb7cf7f873ba690bd973bcebf5327e16
Gabriel <gabriel.g2d2@gmail.com>
2024-12-06 12:57:48 -0300

    feat: ✨ Add NODE_ENV variable to environment configuration and update .env files

--- src/env/index.ts
+++ src/env/index.ts
@@ -3,6 +3,7 @@ import "dotenv/config";
 import { z } from "zod";
 
 const envSchema = z.object({
+  NODE_ENV: z.enum(["development", "production", "test"]).default("production"),
   DATABASE_URL: z.string(),
   PORT: z
     .string()
@@ -10,4 +11,11 @@ const envSchema = z.object({
     .default("3333"),
 });
 
-export const env = envSchema.parse(process.env);
+const _env = envSchema.safeParse(process.env);
+
+if (!_env.success) {
+  console.error("Invalid environment variables => ", _env.error.format());
+  throw new Error("Invalid environment variables");
+}
+
+export const env = _env.data;
