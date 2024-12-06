24481637ade0402e16a08c1e579b3a36ad9c4044
Gabriel <gabriel.g2d2@gmail.com>
2024-12-06 16:13:57 -0300

    feat: ✨ Enhance transaction creation with validation and error handling

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -6,45 +6,56 @@ import { ITypeMessageGlobal } from "../global/types/typeMessage";
 
 export async function transactionsRoutes(app: FastifyInstance) {
   app.post("/transactions", async (request, response) => {
-    const createTransactionSchema = z.object({
-      title: z.string(),
-      amount: z.number(),
-      type: z.enum(["credit", "debit"]),
-    });
+    try {
+      const createTransactionSchema = z.object({
+        title: z.string(),
+        amount: z.number(),
+        type: z.enum(["credit", "debit"]),
+      });
 
-    const verifyBody = createTransactionSchema.safeParse(request.body);
+      const verifyBody = createTransactionSchema.safeParse(request.body);
 
-    if (!verifyBody.success) {
-      return response.status(400).send({
+      if (!verifyBody.success) {
+        return response.status(400).send({
+          data: null,
+          message: {
+            en: "Invalid data",
+            pt: "Dados inválidos",
+          },
+          typeMessage: ITypeMessageGlobal.ERROR,
+          errors: verifyBody.error.errors,
+        });
+      }
+
+      const body = verifyBody.data;
+
+      await knex("transactions").insert({
+        id: randomUUID(),
+        title: body.title,
+        amount: body.type === "credit" ? body.amount : -body.amount,
+      });
+
+      return response.status(201).send({
+        data: {
+          title: body.title,
+          amount: body.amount,
+          type: body.type,
+        },
+        message: {
+          en: "Transaction created successfully",
+          pt: "Transação criada com sucesso",
+        },
+        typeMessage: ITypeMessageGlobal.SUCCESS,
+      });
+    } catch (error) {
+      return response.status(500).send({
         data: null,
         message: {
-          en: "Invalid data",
-          pt: "Dados inválidos",
+          en: "Internal server error",
+          pt: "Erro interno do servidor",
         },
         typeMessage: ITypeMessageGlobal.ERROR,
-        errors: verifyBody.error.errors,
       });
     }
-
-    const body = verifyBody.data;
-
-    await knex("transactions").insert({
-      id: randomUUID(),
-      title: body.title,
-      amount: body.type === "credit" ? body.amount : -body.amount,
-    });
-
-    return response.status(201).send({
-      data: {
-        title: body.title,
-        amount: body.amount,
-        type: body.type,
-      },
-      message: {
-        en: "Transaction created successfully",
-        pt: "Transação criada com sucesso",
-      },
-      typeMessage: ITypeMessageGlobal.SUCCESS,
-    });
   });
 }
