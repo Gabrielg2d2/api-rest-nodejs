29651db6a63d37a971b9bca09ae56ebd8f186319
Gabriel <gabriel.g2d2@gmail.com>
2024-12-06 12:51:15 -0300

    feat: ✨ Add PORT validation and default value to environment schema

--- src/env/index.ts
+++ src/env/index.ts
@@ -4,6 +4,10 @@ import { z } from "zod";
 
 const envSchema = z.object({
   DATABASE_URL: z.string(),
+  PORT: z
+    .string()
+    .transform((value) => Number(value))
+    .default("3333"),
 });
 
 export const env = envSchema.parse(process.env);
