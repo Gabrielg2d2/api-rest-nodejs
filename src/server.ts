6d50bce190be1f95f84b0583bfd3cb94ade38b9e
Gabriel <gabriel.g2d2@gmail.com>
2024-12-06 15:36:57 -0300

    feat: ✨ Register transactions routes and remove deprecated hello endpoint

--- src/server.ts
+++ src/server.ts
@@ -1,26 +1,12 @@
 import Fastify from "fastify";
-import { knex } from "./database";
 import { env } from "./env";
+import { transactionsRoutes } from "./routes/transactions";
 
 const app = Fastify({
   logger: true,
 });
 
-app.get("/hello", async function handler(request, response) {
-  // const newTransaction = await knex("transactions")
-  //   .insert({
-  //     id: crypto.randomUUID(),
-  //     title: "Transação Teste",
-  //     amount: 400,
-  //   })
-  //   .returning("*");
-
-  // return newTransaction;
-
-  const transactions = await knex("transactions").select("*");
-
-  return transactions;
-});
+app.register(transactionsRoutes);
 
 app
   .listen({
