ade7f4140ac556eeda1ffb5260e67380b6a54624
Gabriel <gabriel.g2d2@gmail.com>
2024-12-09 12:50:45 -0300

    test: restructure and expand transaction tests for clarity and coverage

--- src/tests/transactions/createTransaction.test.ts
+++ src/tests/transactions/createTransaction.test.ts
@@ -11,14 +11,66 @@ afterAll(async () => {
   await app.close();
 });
 
-describe("Transactions", () => {
-  test("Create a new transaction", async () => {
-    const response = await request(app.server).post("/transactions").send({
-      title: "car",
-      amount: 3000,
-      type: "credit",
+describe("Routes Transactions", () => {
+  describe("Create Transaction", () => {
+    test("Create a new transaction", async () => {
+      const response = await request(app.server).post("/transactions").send({
+        title: "car",
+        amount: 3000,
+        type: "credit",
+      });
+
+      expect(response.statusCode).toBe(201);
     });
 
-    expect(response.statusCode).toBe(201);
+    test("Create a new transaction with invalid amount", async () => {
+      const response = await request(app.server).post("/transactions").send({
+        title: "car",
+        amount: "invalid",
+        type: "credit",
+      });
+
+      expect(response.statusCode).toBe(400);
+    });
+  });
+
+  describe("List Transactions", () => {
+    test("List transactions", async () => {
+      const createTransactionResponse = await request(app.server)
+        .post("/transactions")
+        .send({
+          title: "car",
+          amount: 3000,
+          type: "credit",
+        });
+
+      const cookies = createTransactionResponse.get("Set-Cookie");
+
+      if (!cookies) {
+        throw new Error("No cookies set in response");
+      }
+
+      const response = await request(app.server)
+        .get("/transactions")
+        .set("Cookie", cookies);
+
+      expect(response.statusCode).toBe(200);
+      expect(response.body.data.transactions).toHaveLength(1);
+      expect(response.body).toEqual({
+        data: {
+          transactions: [
+            {
+              created_at: expect.any(String),
+              id: expect.any(String),
+              session_id: expect.any(String),
+              amount: 3000,
+              title: "car",
+            },
+          ],
+        },
+        message: { en: "", pt: "" },
+        typeMessage: "success",
+      });
+    });
   });
 });
