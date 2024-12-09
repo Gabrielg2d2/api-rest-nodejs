0d6cda64eabf1354f1a94d7e14fe3ecb93b617e8
Gabriel <gabriel.g2d2@gmail.com>
2024-12-09 11:38:44 -0300

    refactor: ✨ Simplify server initialization by importing app from app module

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,5 @@
-import cookie from "@fastify/cookie";
-import Fastify from "fastify";
+import { app } from "./app";
 import { env } from "./env";
-import { transactionsRoutes } from "./routes/transactions";
-
-const app = Fastify();
-
-app.register(cookie);
-
-app.register(transactionsRoutes, {
-  prefix: "/transactions",
-});
 
 app
   .listen({
